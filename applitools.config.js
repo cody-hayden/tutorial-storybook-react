d4f82d64508265bdb581bff813acc42dee312317
codydhayden <cody.hayden@applitools.com>
2022-02-25 10:07:54 -0500

    Update applitools.config.js

--- applitools.config.js
+++ applitools.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   concurrency: 1,
-  waitBeforeCapture: async () => {
+  /*waitBeforeCapture: async () => {
     await new Promise(r => setTimeout(r, 2000));
     return true;
-  },
+  },*/
   showLogs: true
 }
 
